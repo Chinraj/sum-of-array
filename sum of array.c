84208daa803ae59685e9d26cafd8ee05e30c54ee
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-07-01 15:46:52 +0530

    Update sum of array.c

--- sum of array.c
+++ sum of array.c
@@ -2,11 +2,13 @@
 #include<conio.h>
 void main()
 {
-    int a[4],b;
+    int a[4];
+    float b;
     clrscr();
     printf("enter the numbers\n");
     scanf("%d%d%d%d",&a[1],&a[2],&a[3],&a[4]);
     b=a[1]+a[2]+a[3]+a[4];
-    printf("%d",b);
-    getch();
-    }
+    b=b/4;
+printf("%.1f",b);
+getch();
+}
