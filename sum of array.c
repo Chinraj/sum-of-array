553912b61385e9646c1229deb7b1e2ed3b731c9f
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-07-01 15:43:35 +0530

    Create sum of array.c

---
+++ sum of array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[4],b;
+    clrscr();
+    printf("enter the numbers\n");
+    scanf("%d%d%d%d",&a[1],&a[2],&a[3],&a[4]);
+    b=a[1]+a[2]+a[3]+a[4];
+    printf("%d",b);
+    getch();
+    }
